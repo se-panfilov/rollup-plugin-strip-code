e43a1d0abaa001d3b21cc7ce6622a2dae9c23bac
Sergei <se-panfilov@ya.ru>
2017-11-30 14:30:16 +0800

    added tests

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,5 +1,96 @@
 const stripCode = require('../index')
 
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4)
+
+describe('stripCode.', () => {
+
+  test('should has proper name', () => {
+    expect(stripCode().name).toBe('stripCode')
+  })
+
+  test('should return proper oberct', () => {
+    expect(typeof stripCode()).toBe('object')
+  })
+
+  describe('default settings.', () => {
+
+    test('should remove code', () => {
+      const obj = stripCode()
+
+      const source = `whatever\n
+      /* start_comment */ \n
+      text to remove\n
+      /* end_comment */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe('whatever')
+    })
+
+    test('should not remove anything', () => {
+      const obj = stripCode()
+
+      const source = `whatever\n
+      /* whatever_comment */ \n
+      text to remove\n
+      /* ends_comment */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe(source)
+    })
+
+  })
+
+  describe('override settings.', () => {
+
+    test('should remove code with overrided options', () => {
+      const obj = stripCode({
+        start_comment: 'START.TESTS_ONLY',
+        end_comment: 'END.TESTS_ONLY'
+      })
+
+      const source = `whatever\n
+      /* START.TESTS_ONLY */ \n
+      text to remove\n
+      /* END.TESTS_ONLY */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe('whatever')
+    })
+
+    test('should remove code with standart comments', () => {
+      const obj = stripCode({
+        start_comment: 'START.TESTS_ONLY',
+        end_comment: 'END.TESTS_ONLY'
+      })
+
+      const source = `whatever\n
+      /* start_comment */ \n
+      text to remove\n
+      /* end_comment */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe(source)
+    })
+
+    test('should not remove anything', () => {
+      const obj = stripCode({
+        start_comment: 'START.TESTS_ONLY',
+        end_comment: 'END.TESTS_ONLY'
+      })
+
+      const source = `whatever\n
+      /* whatever_comment */ \n
+      text to remove\n
+      /* ends_comment */`
+
+      const result = obj.transform(source).code
+
+      expect(result.trim()).toBe(source)
+    })
+
+  })
+
 })
