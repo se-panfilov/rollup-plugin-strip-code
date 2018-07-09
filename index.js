30fa4e6488dd37fa79da33145f838bd515c4ef7a
Sergei Panfilov <sergei.panfilov@powerhouse.nl>
2018-07-09 10:42:18 +0200

    fixed deps vulnerability

--- index.js
+++ index.js
@@ -4,28 +4,29 @@ const MagicString = require('magic-string')
 const START_COMMENT = 'start_comment'
 const END_COMMENT = 'end_comment'
 
-function stripCode(options = {}) {
-  const filter = createFilter(options.include, options.exclude)
+function stripCode (options = {}) {
+  const {include, exclude, sourceMap, pattern, sourcemap} = options
+
+  const filter = createFilter(include, exclude)
 
   return {
     name: 'stripCode',
 
-    transform(source, id) {
+    transform (source, id) {
       if (!filter(id)) return
 
       const startComment = options.start_comment || START_COMMENT
       const endComment = options.end_comment || END_COMMENT
       const defaultPattern = new RegExp(`([\\t ]*\\/\\* ?${startComment} ?\\*\\/)[\\s\\S]*?(\\/\\* ?${endComment} ?\\*\\/[\\t ]*\\n?)`, 'g')
-      const pattern = options.pattern || defaultPattern
       let map
-      let code = source.replace(pattern, '')
+      let code = source.replace(pattern || defaultPattern, '')
 
-      if (options.sourceMap !== false && options.sourcemap !== false) {
+      if (sourceMap !== false && sourcemap !== false) {
         const magicString = new MagicString(code)
-        map = magicString.generateMap({ hires: true })
+        map = magicString.generateMap({hires: true})
       }
 
-      return { code, map }
+      return {code, map}
     }
   }
 }
